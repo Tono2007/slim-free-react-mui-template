8abc1d3c9fca527b939d7d8fef9672bf4101d70e
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-03-22 16:34:42 -0600

    [ADD] Redux setup
    -redux toolkit

---
+++ src/redux/store.jsx
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+	Provider as ReduxProvider,
+	useSelector,
+	useDispatch,
+} from 'react-redux';
+
+const store = configureStore({
+	devTools: process.env.NODE_ENV !== 'production',
+	reducer: {},
+});
+
+function Provider({ children }) {
+	return <ReduxProvider store={store}>{children}</ReduxProvider>;
+}
+
+export { useSelector, useDispatch };
+export default Provider;
