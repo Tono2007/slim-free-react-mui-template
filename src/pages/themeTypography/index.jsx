e37b4cd6b71b5643f4ddc4c5f53e7c44baf4c760
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-04-14 10:20:07 -0600

    [FEAT] ThemeTypography Page

---
+++ src/pages/themeTypography/index.jsx
@@ -0,0 +1,31 @@
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
+import PageHeader from '@/components/pageHeader';
+import BasicTypography from './basicTypography';
+
+function ThemeTypography() {
+	return (
+		<>
+			<PageHeader title="Typography">
+				<Breadcrumbs
+					aria-label="breadcrumb"
+					sx={{ textTransform: 'uppercase' }}
+				>
+					<Link underline="hover" href="#!">
+						Inicio
+					</Link>
+					<Link underline="hover" href="#!">
+						Theme
+					</Link>
+					<Typography color="text.tertiary">Typography</Typography>
+				</Breadcrumbs>
+			</PageHeader>
+			<BasicTypography />
+		</>
+	);
+}
+
+export default ThemeTypography;
