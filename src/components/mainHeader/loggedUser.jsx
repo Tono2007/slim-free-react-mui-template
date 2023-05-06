204d7920c8bc6aa4e293990a640b9181726ed031
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-05-06 10:36:56 -0600

    [FEAT] header user menu badges
    -menu items & badges

--- src/components/mainHeader/loggedUser.jsx
+++ src/components/mainHeader/loggedUser.jsx
@@ -11,13 +11,21 @@ import Menu from '@mui/material/Menu';
 import MenuList from '@mui/material/MenuList';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
 
 // Icons
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
-
+import LockPersonOutlinedIcon from '@mui/icons-material/LockPersonOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PaymentOutlinedIcon from '@mui/icons-material/PaymentOutlined';
+import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import DraftsOutlinedIcon from '@mui/icons-material/DraftsOutlined';
+import TaskOutlinedIcon from '@mui/icons-material/TaskOutlined';
+import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
 // assets
 import avatar2 from '@/assets/images/avatars/avatar_13.jpg';
 
@@ -54,43 +62,7 @@ function LoggedUser() {
 				open={Boolean(anchorEl)}
 				onClose={handleClose}
 			>
-				<MenuList
-					sx={{
-						p: 1,
-					}}
-				>
-					<Typography variant="subtitle1" textAlign="center">
-						Elizabeth Lumaad Olsen
-					</Typography>
-					<Typography variant="subtitle2" textAlign="center">
-						Executive Director
-					</Typography>
-					<Divider
-						sx={{
-							borderColor: 'primary.light',
-							my: 1,
-						}}
-					/>
-
-					<MenuItem onClick={handleClose} to="/profile" component={RouterLink}>
-						<ListItemIcon>
-							<Person2OutlinedIcon fontSize="small" />
-						</ListItemIcon>
-						Perfil
-					</MenuItem>
-					<MenuItem onClick={handleClose} to="/profile/edit" component={RouterLink}>
-						<ListItemIcon>
-							<ManageAccountsOutlinedIcon fontSize="small" />
-						</ListItemIcon>
-						Editar Perfil
-					</MenuItem>
-					<MenuItem onClick={handleClose} component={RouterLink} to="/">
-						<ListItemIcon>
-							<ExitToAppIcon fontSize="small" />
-						</ListItemIcon>
-						Cerrar Sesión
-					</MenuItem>
-				</MenuList>
+				<UserMenu handleClose={handleClose} />
 			</Menu>
 			<Stack height="100%" direction="row" flex={1} justifyContent="flex-end" alignItems="center" spacing={1}>
 				<NotificationsButton />
@@ -144,6 +116,123 @@ function LoggedUser() {
 				</ButtonBase>
 			</Stack>
 		</>
+	);
+}
+
+function UserMenu({ handleClose }) {
+	return (
+		<MenuList
+			sx={{
+				p: 1,
+			}}
+		>
+			<Stack px={3}>
+				<Typography variant="subtitle1" textAlign="center">
+					Elizabeth Lumaad Olsen
+				</Typography>
+				<Typography variant="subtitle2" textAlign="center">
+					Executive Director
+				</Typography>
+			</Stack>
+			<Divider
+				sx={{
+					borderColor: 'primary.light',
+					my: 1,
+				}}
+			/>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<NotificationsNoneOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Notifications <Badge color="info.main" count={10} />
+			</MenuItem>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<DraftsOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Messages
+				<Badge color="success.main" count={5} />
+			</MenuItem>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<TaskOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Tasks <Badge color="error.main" count={23} />
+			</MenuItem>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<CommentOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Comments <Badge color="warning.main" count={16} />
+			</MenuItem>
+			<Divider
+				sx={{
+					borderColor: 'primary.light',
+					my: 1,
+				}}
+			/>
+			<MenuItem onClick={handleClose} to="/profile" component={RouterLink}>
+				<ListItemIcon>
+					<Person2OutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Profile
+			</MenuItem>
+
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<SettingsOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Settings
+			</MenuItem>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<PaymentOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Payments
+			</MenuItem>
+			<MenuItem onClick={handleClose} to="/" component={RouterLink}>
+				<ListItemIcon>
+					<SummarizeOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Projects
+			</MenuItem>
+			<Divider
+				sx={{
+					borderColor: 'primary.light',
+					my: 1,
+				}}
+			/>
+			<MenuItem onClick={handleClose} component={RouterLink} to="/">
+				<ListItemIcon>
+					<LockPersonOutlinedIcon fontSize="small" />
+				</ListItemIcon>
+				Lock Account
+			</MenuItem>
+			<MenuItem onClick={handleClose} component={RouterLink} to="/">
+				<ListItemIcon>
+					<ExitToAppIcon fontSize="small" />
+				</ListItemIcon>
+				Logout
+			</MenuItem>
+		</MenuList>
+	);
+}
+
+function Badge({ color, count }) {
+	return (
+		<Box
+			ml={1}
+			bgcolor={color}
+			color="primary.contrastText"
+			height={20}
+			width={20}
+			fontSize="body1"
+			borderRadius="50%"
+			display="grid"
+			sx={{ placeItems: 'center' }}
+		>
+			{count}
+		</Box>
 	);
 }
 export default LoggedUser;
